8582dcfe031394c0e77cfeb5ce608d2d98853604
mohit <connectwithlastdev@gmail.com>
2024-05-14 22:05:29 +0530

    correct index file to add metadata

--- index.js
+++ index.js
@@ -1,41 +1,76 @@
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
-import { Connection, Keypair } from '@solana/web3.js';
+// run for one time, 2nd time error
+import * as mpl from "@metaplex-foundation/mpl-token-metadata";
+import * as web3 from "@solana/web3.js";
+import * as anchor from '@project-serum/anchor';
+import fs from "fs";
 
-const RPC_ENDPOINT = 'https://api.devnet.solana.com';
-const connection = new Connection(RPC_ENDPOINT);
+export function loadWalletKey(keypairFile) {
+    const loaded = web3.Keypair.fromSecretKey(
+        new Uint8Array(JSON.parse(fs.readFileSync(keypairFile).toString())),
+    );
+    return loaded;
+}
 
-const umi = createUmi(RPC_ENDPOINT).use(mplTokenMetadata());
+const INITIALIZE = true;
+const METADATA_PROGRAM_ID = new web3.PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
 
+async function main() {
+    console.log("Let's name some tokens!");
+    const myKeypair = loadWalletKey("id.json");
+    // Token Address-
+    const mint = new web3.PublicKey("G4yKvV7iSf1PBqaoXnRxVQ8TyhSuxiNKXhfU967PG8mr");
 
+    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("metadata"));
+    const seed2 = Buffer.from(METADATA_PROGRAM_ID.toBuffer());
+    const seed3 = Buffer.from(mint.toBytes());
+    const [metadataPDA, _bump] = web3.PublicKey.findProgramAddressSync([seed1, seed2, seed3], METADATA_PROGRAM_ID);
 
+    const accounts = {
+        metadata: metadataPDA,
+        mint,
+        mintAuthority: myKeypair.publicKey,
+        payer: myKeypair.publicKey,
+        updateAuthority: myKeypair.publicKey,
+    };
 
-const walletKeypair = Keypair.fromSecretKey(new Uint8Array([99,120,25,145,105,76,121,215,60,232,217,229,79,212,106,158,183,59,203,196,187,13,101,204,120,145,215,57,220,82,49,184,89,10,219,90,60,34,249,51,28,187,195,111,211,176,58,116,17,87,197,148,18,188,179,251,109,14,47,40,87,42,89,215]
-));
+    const dataV2 = {
+        name: "My Unique Token",
+        symbol: "MUT",
+        uri: "https://raw.githubusercontent.com/last-developer/metadata/main/metadata.json",
+        sellerFeeBasisPoints: 0,
+        creators: null,
+        collection: null,
+        uses: null
+    };
 
+    let ix;
+    if (INITIALIZE) {
+        const args = {
+            createMetadataAccountArgsV3: {
+                data: dataV2,
+                isMutable: true,
+                collectionDetails: null
+            }
+        };
+        ix = mpl.createCreateMetadataAccountV3Instruction(accounts, args);
+    } else {
+        const args = {
+            updateMetadataAccountArgsV2: {
+                data: dataV2,
+                updateAuthority: myKeypair.publicKey,
+                primarySaleHappened: false
+            }
+        };
+        ix = mpl.createUpdateMetadataAccountV2Instruction(accounts, args);
+    }
+    const tx = new web3.Transaction();
+    tx.add(ix);
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+    const payerBalance = await connection.getBalance(myKeypair.publicKey);
+    console.log(`Payer Balance: ${payerBalance / web3.LAMPORTS_PER_SOL} SOL`);
+    console.log(myKeypair.publicKey);
+    const txid = await web3.sendAndConfirmTransaction(connection, tx, [myKeypair]);
+    console.log("Transaction ID:", txid);
+}
 
-
-
-import { PublicKey } from '@solana/web3.js';
-import { createNft } from '@metaplex-foundation/mpl-token-metadata';
-
-const tokenMintAddress = new PublicKey('mGuZw1y3mnDDgj7zD7vUXyah2uan1C26GP2bEdrLFTx');
-
-await createNft(umi, {
-  mint: tokenMintAddress,
-  name: 'Minimalist',
-  uri: 'https://example.com/my-nft.json',
-  sellerFeeBasisPoints: 500, // 5%
-  creators: [{
-    address: walletKeypair.publicKey.toString(),
-    share: 100
-  }]
-}).sendAndConfirm(walletKeypair);
-
-
-
-
-import { fetchDigitalAsset } from '@metaplex-foundation/mpl-token-metadata';
-
-const asset = await fetchDigitalAsset(umi, tokenMintAddress);
-console.log(asset);
+main().catch(console.error);
